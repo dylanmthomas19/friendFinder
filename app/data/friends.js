d0f5e59b366a08c9a886433c9de62872c3b8935e
Dylan Thomas <azelthedemon@gmail.com>
2018-07-26 19:59:35 -0600

    fantasy friend finder fixed

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,99 +6,99 @@
 
 var friends = [
     {
-      name: "Ahmed",
-      photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
-      scores: [
-        "5",
-        "1",
-        "4",
-        "4",
-        "5",
-        "1",
-        "2",
-        "5",
-        "4",
-        "1"
-      ]
-    },
-    {
-      name: "Jacob Deming",
-      photo: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+      name: "Mohinder",
+      photo: "../public/images/guy_1",
       scores: [
         "4",
-        "2",
+        "3",
+        "1",
         "5",
-        "1",
-        "3",
         "2",
-        "2",
-        "1",
         "3",
+        "1",
+        "4",
+        "4",
         "2"
       ]
     },
     {
-      name: "Jeremiah Scanlon",
-      photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+      name: "Rebecca",
+      photo: "../public/images/gal_1",
       scores: [
         "5",
-        "2",
-        "2",
+        "3",
+        "1",
+        "1",
+        "5",
         "2",
         "4",
-        "1",
-        "3",
         "2",
         "5",
+        "1"
+      ]
+    },
+    {
+      name: "Jeremiah (Not a bullfrog)",
+      photo: "../public/images/guy_2",
+      scores: [
+        "3",
+        "5",
+        "1",
+        "3",
+        "5",
+        "2",
+        "4",
+        "2",
+        "1",
         "5"
       ]
     },
     {
-      name: "Louis T. Delia",
-      photo: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+      name: "Louisa",
+      photo: "../public/images/gal_2",
       scores: [
-        "3",
-        "3",
-        "4",
-        "2",
         "2",
         "1",
-        "3",
-        "2",
-        "2",
-        "3"
-      ]
-    },
-    {
-      name: "Lou Ritter",
-      photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
-      scores: [
-        "4",
+        "5",
         "3",
         "4",
         "1",
         "5",
         "2",
-        "5",
-        "3",
         "1",
         "4"
       ]
     },
     {
-      name: "Jordan Biason",
-      photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+      name: "Nathan",
+      photo: "../public/images/guy_3",
       scores: [
-        "4",
-        "4",
+        "5",
+        "1",
         "2",
+        "4",
         "3",
-        "2",
-        "2",
         "3",
-        "2",
+        "1",
         "4",
-        "5"
+        "2",
+        "3"
+      ]
+    },
+    {
+      name: "Harriette",
+      photo: "../public/images/gal_3",
+      scores: [
+        "5",
+        "2",
+        "1",
+        "4",
+        "5",
+        "3",
+        "4",
+        "1",
+        "3",
+        "2"
       ]
     }
   
